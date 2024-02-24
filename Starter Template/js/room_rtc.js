4a622e94e5036925f17740165ba133d6abd50f63
lazydev01 <arpeetbhavsar01@gmail.com>
2024-02-24 15:25:29 +0530

    Added Controls

--- Starter Template/js/room_rtc.js
+++ Starter Template/js/room_rtc.js
@@ -2,7 +2,7 @@ import { AGORA_APP_ID } from "./env.js";
 
 let uid = sessionStorage.getItem("uid");
 if(!uid){
-    let uid = String(Math.floor(Math.random()*10000))
+    uid = String(Math.floor(Math.random()*10000))
     sessionStorage.setItem("uid", uid);
 }
 
@@ -50,8 +50,6 @@ let expandVideoFrame = (e) => {
     displayFrame.appendChild(e.currentTarget);
     userIdInDisplayFrame = e.currentTarget.id;
   
-    console.log(videoFrames);
-  
     for(let i=0; i<videoFrames.length; i++){
       if(videoFrames[i].id !== userIdInDisplayFrame){
         videoFrames[i].style.height = '100px';
@@ -61,7 +59,11 @@ let expandVideoFrame = (e) => {
   }
 
 let joinStream = async() => {
-    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks();
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks({}, {encoderConfig : {
+        width : {min : 640, ideal : 1920, max : 1920},
+        height : {min : 480, ideal : 1080, max : 1080}
+    }
+    });
     let player = `<div class="video-container" id="user-container-${uid}">
                     <div class="video-player" id="user-${uid}"></div>
                 </div>`;
@@ -75,7 +77,6 @@ let joinStream = async() => {
 }
 
 let handleUserPublished = async(user, mediaType) => {
-    console.log("new user published");
     remoteUsers[user.uid] = user;
     await client.subscribe(user, mediaType);
 
@@ -90,6 +91,13 @@ let handleUserPublished = async(user, mediaType) => {
         document.getElementById(`user-container-${user.uid}`).addEventListener('click', expandVideoFrame);
     }
 
+
+    if(displayFrame.style.display){
+        let currentVideoPlayer = document.getElementById(`user-container-${user.uid}`);
+        currentVideoPlayer.style.height = '100px';
+        currentVideoPlayer.style.width = '100px';
+    }
+
     
 
     if(mediaType === 'video'){
@@ -102,8 +110,52 @@ let handleUserPublished = async(user, mediaType) => {
 }
 
 let handleUserLeft = async(user) => {
+
+    if(userIdInDisplayFrame === `user-container-${user.uid}`){
+        displayFrame.style.display = null;
+
+        let videoFrames = document.getElementsByClassName(`video-container`);
+
+        for(let i=0; i<videoFrames.length; i++){
+            videoFrames[i].style.width = `300px`;
+            videoFrames[i].style.height = `300px`;
+        }
+    }
     delete remoteUsers[user.uid];
     document.getElementById(`user-container-${user.uid}`).remove();
 }
 
+let hideDisplayFrame = () => {
+    userIdInDisplayFrame = null;
+    displayFrame.style.display = null;
+
+    let child = displayFrame.children[0];
+    document.getElementById(`streams-container`).appendChild(child);
+
+    for(let i=0; i<videoFrames.length; i++){
+        videoFrames[i].display.height = `300px`;
+        videoFrames[i].display.width= `300px`;
+    }
+}
+
+displayFrame.addEventListener('click', hideDisplayFrame);
+
+let toggleControls = async(e) => {
+    let cameraCtrl = false;
+    let button = e.currentTarget;
+    if(button.id === 'camera-btn'){
+        cameraCtrl = true;
+    }
+    if(localTracks[cameraCtrl ? 1 : 0].muted){
+        localTracks[cameraCtrl ? 1 : 0].setMuted(false);
+        button.classList.add('active');
+    }
+    else{
+        localTracks[cameraCtrl ? 1 : 0].setMuted(true);
+        button.classList.remove('active');
+    }
+}
+
+document.getElementById('camera-btn').addEventListener("click", toggleControls);
+document.getElementById('mic-btn').addEventListener("click", toggleControls);
 joinRoomInit();
